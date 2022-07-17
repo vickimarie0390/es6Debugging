9fa3b9cfa0156a2dc2c8038ab3b64b76f5c28a93
Victoria <vickimarji@icloud.com>
2022-07-17 13:15:45 -0400

    new push

---
+++ app.js
@@ -0,0 +1,67 @@
+var x = 7;
+var y = 3;
+
+
+function sum(param1, param2) {
+    console.log(param1 + param2);
+}
+
+sum(x, y);
+
+
+
+var x = 2;
+var y = 9;
+
+function sum(param1, param2) {
+    console.log(param1 - param2);
+
+}
+
+sum(x, y);
+
+
+var x = 5;
+var y = 4;
+
+function sum(param1, param2) {
+console.log(param1 * param2);
+
+}
+
+sum(x, y);
+
+
+
+var x = 4;
+var y = 2;
+
+function sum(param1, param2) {
+console.log(param1 / param2);
+
+}
+
+sum(x, y);
+
+
+
+let add = (x, y) => x + y;
+
+console.log(add(7, 3));
+
+
+
+let differnce = (x, y) => x - y;
+
+console.log(differnce(2, 9));
+
+
+
+let multiply = (x, y) => x * y;
+
+console.log(multiply(5, 4));
+
+
+
+let divide = (x, y) => x / y;
+console.log(divide(4, 2));
